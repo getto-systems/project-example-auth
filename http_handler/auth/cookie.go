6d8beac85bc2213e01216c69a93ae93451d178f6
shun <shun.fix9@gmail.com>
2020-05-23 19:23:04 +0000

    refactor: union http_handler/auth

--- http_handler/auth/cookie.go
+++ http_handler/auth/cookie.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/getto-systems/project-example-id/auth"
 
+	"github.com/getto-systems/project-example-id/token"
 	"github.com/getto-systems/project-example-id/user"
 )
+
+const COOKIE_AUTH_TOKEN = "Getto-Example-Auth-Token"
 
 type CookieSetter struct {
 	ResponseWriter http.ResponseWriter
@@ -21,6 +24,44 @@ type Cookie struct {
 	Value string
 }
 
+func (setter CookieSetter) setAuthTokenCookie(token auth.Token) {
+	setter.setTicketCookie(token.TicketToken)
+	setter.setAwsCloudFrontCookie(token.AwsCloudFrontToken)
+}
+
+func getTicketCookie(r *http.Request) (token.TicketToken, error) {
+	cookie, err := r.Cookie(COOKIE_AUTH_TOKEN)
+	if err != nil {
+		return nil, err
+	}
+
+	return token.TicketToken(cookie.Value), nil
+}
+
+func (setter CookieSetter) setTicketCookie(ticketToken token.TicketToken) {
+	setter.setCookie(&Cookie{
+		Name:  COOKIE_AUTH_TOKEN,
+		Value: string(ticketToken),
+	})
+}
+
+func (setter CookieSetter) setAwsCloudFrontCookie(token token.AwsCloudFrontToken) {
+	setter.setCookie(&Cookie{
+		Name:  "CloudFront-Key-Pair-Id",
+		Value: string(token.KeyPairID),
+	})
+
+	setter.setCookie(&Cookie{
+		Name:  "CloudFront-Policy",
+		Value: string(token.Policy),
+	})
+
+	setter.setCookie(&Cookie{
+		Name:  "CloudFront-Signature",
+		Value: string(token.Signature),
+	})
+}
+
 func (setter CookieSetter) setCookie(cookie *Cookie) {
 	http.SetCookie(setter.ResponseWriter, &http.Cookie{
 		Name:  cookie.Name,
@@ -33,9 +74,4 @@ func (setter CookieSetter) setCookie(cookie *Cookie) {
 		Secure:   true,
 		HttpOnly: true,
 	})
-}
-
-func (setter CookieSetter) setAuthTokenCookie(token auth.Token) {
-	setter.setTicketCookie(token.TicketToken)
-	setter.setAwsCloudFrontCookie(token.AwsCloudFrontToken)
 }
