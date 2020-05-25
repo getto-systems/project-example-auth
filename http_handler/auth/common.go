4fa1f5eb96aca999f16c433313fee577e58b14e2
shun <shun.fix9@gmail.com>
2020-05-25 12:13:56 +0000

    refactor: http_handler/auth/common

--- http_handler/auth/common.go
+++ http_handler/auth/common.go
@@ -6,9 +6,27 @@ import (
 
 	"github.com/getto-systems/project-example-id/auth"
 
+	"github.com/getto-systems/project-example-id/token"
+	"github.com/getto-systems/project-example-id/user"
+
 	"errors"
 	"fmt"
 )
+
+const COOKIE_AUTH_TOKEN = "Getto-Example-Auth-Token"
+
+type CookieDomain string
+
+type Cookie struct {
+	Name  string
+	Value string
+}
+
+type CookieSetter struct {
+	ResponseWriter http.ResponseWriter
+	CookieDomain   CookieDomain
+	Ticket         user.Ticket
+}
 
 var ErrBodyNotSent = errors.New("body not sent")
 var ErrBodyParseFailed = errors.New("body parse failed")
@@ -41,4 +59,61 @@ func httpStatusCode(err error) int {
 	default:
 		return http.StatusInternalServerError
 	}
+}
+
+func setAuthTokenCookie(w http.ResponseWriter, cookieDomain CookieDomain, ticket user.Ticket, token auth.Token) {
+	setter := CookieSetter{
+		w,
+		cookieDomain,
+		ticket,
+	}
+	setter.setTicketCookie(token.RenewToken)
+	setter.setAwsCloudFrontCookie(token.AwsCloudFrontToken)
+}
+
+func getRenewToken(r *http.Request) (token.RenewToken, error) {
+	cookie, err := r.Cookie(COOKIE_AUTH_TOKEN)
+	if err != nil {
+		return nil, err
+	}
+
+	return token.RenewToken(cookie.Value), nil
+}
+
+func (setter CookieSetter) setTicketCookie(renewToken token.RenewToken) {
+	setter.setCookie(&Cookie{
+		Name:  COOKIE_AUTH_TOKEN,
+		Value: string(renewToken),
+	})
+}
+
+func (setter CookieSetter) setAwsCloudFrontCookie(token token.AwsCloudFrontToken) {
+	setter.setCookie(&Cookie{
+		Name:  "CloudFront-Key-Pair-Id",
+		Value: string(token.KeyPairID),
+	})
+
+	setter.setCookie(&Cookie{
+		Name:  "CloudFront-Policy",
+		Value: string(token.Policy),
+	})
+
+	setter.setCookie(&Cookie{
+		Name:  "CloudFront-Signature",
+		Value: string(token.Signature),
+	})
+}
+
+func (setter CookieSetter) setCookie(cookie *Cookie) {
+	http.SetCookie(setter.ResponseWriter, &http.Cookie{
+		Name:  cookie.Name,
+		Value: cookie.Value,
+
+		Domain:  string(setter.CookieDomain),
+		Path:    "/",
+		Expires: setter.Ticket.Expires(),
+
+		Secure:   true,
+		HttpOnly: true,
+	})
 }
